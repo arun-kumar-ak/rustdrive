05ef633e907621f8534d270d591ea089485f4d7d
arun-kumar-ak-1812 <arun1812.a@gmail.com>
2025-10-01 19:32:42 +0530

    feat(rustdrive-cli): crud operation added

--- rustdrive-cli/src/main.rs
+++ rustdrive-cli/src/main.rs
@@ -13,8 +13,9 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Upload { path: std::path::PathBuf },
-    Download { filename: String },
+    Download { filename: String, path: std::path::PathBuf },
     List,
+    Delete { filenames: Vec<String> },
 }
 
 fn main() -> Result<()> {
@@ -25,11 +26,27 @@ fn main() -> Result<()> {
             modules::file_crud_op::upload(path.as_path())?;
             println!("File uploaded successfully.");
         }
-        Commands::Download { filename } => {
+        Commands::Download { filename, path } => {
             println!("Downloading file: {}", filename);
+            modules::file_crud_op::download(&filename, path.as_path())?;
         }
         Commands::List => {
             println!("Listing files...");
+            if let Ok(all_files) = modules::file_crud_op::get_all_files()  {
+                if all_files.len() == 0 {
+                    println!("No files found.");
+                    return Ok(());
+                }
+
+                for file in all_files.iter() {
+                    println!("fileName: {}, size: {}", file.filename, file.size);
+                }
+            }
+        }
+        Commands::Delete { filenames} => {
+            println!("deleting files...");
+
+            modules::file_crud_op::delete_files(filenames)?;
         }
     }
 
--- rustdrive-core/src/modules/file_crud_op.rs
+++ rustdrive-core/src/modules/file_crud_op.rs
@@ -1,21 +1,19 @@
 use serde::{Serialize, Deserialize};
-use std::{fs, path::Path, io};
+use std::{fs, io, path::{Path, PathBuf}};
+use anyhow::{Error, Ok, Result};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FileMeta {
-    pub id: i32,
     pub filename: String,
     pub size: u64,
 }
 
-pub fn upload(path: &Path) -> io::Result<FileMeta> {
+pub fn upload(path: &Path) -> Result<FileMeta> {
     if !path.is_file() {
-        return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid file path"));
+        return Err(Error::from(io::Error::new(io::ErrorKind::InvalidInput, "Invalid file path")));
     }
 
-    let cwd = std::env::current_dir()?;
-    let storage_folder_name = "storage";
-    let storage_path = cwd.join(storage_folder_name);
+    let storage_path: PathBuf = get_storage_folder_path()?;
 
     if !storage_path.exists() {
         fs::create_dir(&storage_path)?;
@@ -41,8 +39,77 @@ pub fn upload(path: &Path) -> io::Result<FileMeta> {
     let copied = fs::copy(path, &dest)?; // returns number of bytes copied
 
     Ok(FileMeta {
-        id: 0,
         filename: file_name,
         size: copied,
     })
+}
+
+pub fn download(filename: &str, path: &Path) -> Result<()> {
+    let storage_path: PathBuf = get_storage_folder_path()?;
+
+    if !storage_path.exists() {
+        return Err(Error::from(io::Error::new(io::ErrorKind::NotFound, "Storage folder does not exist")));
+    }
+
+    let source = storage_path.join(filename);
+    if !source.exists() {
+        return Err(Error::from(io::Error::new(io::ErrorKind::NotFound, "File not found in storage")));
+    }
+
+    if !path.is_dir() {
+        return Err(Error::from(io::Error::new(io::ErrorKind::InvalidInput, "Invalid destination path")));
+    }
+
+    let dest = path.join(filename);
+    fs::copy(&source, &dest)?;
+
+    println!("File downloaded successfully to {:?}", dest);
+    Ok(())
+}
+
+pub fn get_all_files() -> Result<Vec<FileMeta>> {
+    let storage_path: PathBuf = get_storage_folder_path()?;
+
+    if !storage_path.exists() {
+        return Ok(vec![]); // No files if storage doesn't exist
+    }
+
+    let mut files = Vec::new();
+    for entry in fs::read_dir(storage_path)? {
+        let entry = entry?;
+        let metadata = entry.metadata()?;
+        if metadata.is_file() {
+            files.push(FileMeta {
+                filename: entry.file_name().to_string_lossy().to_string(),
+                size: metadata.len(),
+            });
+        }
+    }
+
+    Ok(files)
+}
+
+pub fn delete_files(file_names: Vec<String>) -> Result<()> {
+    let storage_path = get_storage_folder_path()?;
+
+    for file_name in file_names.iter() {
+        let file_path = storage_path.join(file_name);
+    
+        if !file_path.exists() {
+            Err(Error::from(io::Error::new(io::ErrorKind::NotFound, "File not found in storage")))?
+        }
+    
+        fs::remove_file(file_path)?;
+
+        println!("{} deleted!", file_name);
+    }
+
+    Ok(())
+}
+
+fn get_storage_folder_path() -> Result<PathBuf> {
+    let cwd = std::env::current_dir()?;
+    let storage_folder_name = "storage";
+
+    Ok(cwd.join(storage_folder_name))
 }
